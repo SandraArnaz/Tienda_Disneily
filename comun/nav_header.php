413b65f1aa8ae2a39966ab07d7d499df663c2c2a
Sandra <sandra.arnazdelgado@gmail.com>
2023-02-28 21:23:13 +0100

    Añadido vista y enlaces de pixar, star wars y marvel

--- comun/nav_header.php
+++ comun/nav_header.php
@@ -3,9 +3,9 @@
     <div class="container-fluid">
 
         <a class="navbar-brand" href="disney_vista.php">Disney</a>
-        <a class="navbar-brand" href="#">Pixar</a>
-        <a class="navbar-brand" href="#">Stars Wars</a>
-        <a class="navbar-brand" href="#">Marvel</a>
+        <a class="navbar-brand" href="pixar_vista.php">Pixar</a>
+        <a class="navbar-brand" href="stars_wars_vista.php">Stars Wars</a>
+        <a class="navbar-brand" href="marvel_vista.php">Marvel</a>
 
         <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
