c652975f27a232e3fe944b96c6f645759ea29c2c
Sandra <sandra.arnazdelgado@gmail.com>
2023-02-28 19:20:20 +0100

    Agregados comentarios

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda Disneily - Pagina principal</title>
+    <title>Tienda DisneyForme - Pagina principal</title>
 
     <!-- Bootstap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
@@ -13,7 +13,7 @@
 
 </head>
 <body>
-
+    <!-- Navegabilidad -->
     <header class="header-style-1">
 
         <?php include('comun/top_header.php');?>
@@ -21,6 +21,7 @@
         
     </header>
 
+    <!-- Carousel imagenes -->
     <div id="carouselIndicators" class="carousel slide">
 
         <div class="carousel-indicators">
@@ -58,6 +59,8 @@
         </button>
 
     </div>
+
+    <!-- Seccion de destacados -->
 
 
 
